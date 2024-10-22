941448e211c10eeec1ac19c0dd270601996aae52
nimurr <nimurnerob404@gmail.com>
2024-10-22 15:59:31 +0600

    add

--- src/Components/HomePage/Team.jsx
+++ src/Components/HomePage/Team.jsx
@@ -24,28 +24,28 @@ const Team = () => {
     {
       name: "Md Masipul Islam Siam",
       role: "Senior Advisor",
-      bio: "Expert in employee management and organizational behavior, focused on enhancing team performance and workplace culture.",
+      bio: "Expert in employee management, organizational behavior, enhancing team performance, workplace culture",
       image:
         "https://res.cloudinary.com/nerob/image/upload/v1729584833/ForTech/bsctxespcqdaxhhsj3ci.jpg",
     },
     {
       name: "AS Sabbir",
       role: "Backend Developer",
-      bio: "Skilled in developing dynamic web applications using the MERN stack (MongoDB, Express.js, React.js, Node.js), with a focus on performance and user experience.",
+      bio: "Skilled in developing dynamic web applications using the MERN Stack .",
       image:
         "https://res.cloudinary.com/nerob/image/upload/v1729585451/ForTech/cqkeh1zpaqpxb7nee5lm.png",
     },
     {
       name: "Dipok Roy",
       role: "Web Developer",
-      bio: "Skilled in developing dynamic web applications using the MERN stack (MongoDB, Express.js, React.js, Node.js), with a focus on performance and user experience.",
+      bio: "Skilled in developing dynamic web applications using the MERN Stack .",
       image:
         "https://res.cloudinary.com/nerob/image/upload/v1729589052/ForTech/nlbfboumntb7sgs15pp9.jpg",
     },
     {
       name: "Dipu Debnath",
       role: "Full Stack Developer",
-      bio: "Skilled in developing dynamic web applications using the MERN stack (MongoDB, Express.js, React.js, Node.js), with a focus on performance and user experience.",
+      bio: "Skilled in developing dynamic web applications using the MERN Stack .",
       image:
         "https://res.cloudinary.com/nerob/image/upload/v1729589052/ForTech/uzch77bnwrf4dyy3n1yj.jpg",
     },
@@ -63,26 +63,29 @@ const Team = () => {
       <h2 className="text-center text-3xl font-bold lg:mt-20 mt-10 mb-10">
         OUR TEAM
       </h2>
-      {/* <div>
+      <div>
         <div 
-          className=" sm:w-96 mx-auto my-10  border  overflow-hidden hover:scale-105 duration-300 border-gray-300 rounded-lg bg-white text-center shadow-lg"
+          className=" sm:w-80 mx-auto my-10  border  overflow-hidden hover:scale-105 duration-300 border-gray-300 rounded-lg bg-white text-center shadow-lg"
         >
           <img
             src={teamMembers[0].image}
             alt={teamMembers[0].name}
-            className="w-full mb-5 max-h-[450px]"
+            className="w-full mb-5 rounded border-2 border-primary overflow-hidden"
           />
-          <div className="p-5">
+          <div className="p-3">
             <h2 className="text-2xl font-semibold mb-2">{teamMembers[0].name}</h2>
-            <h3 className="text-blue-500 text-lg mb-4 font-semibold">
+            <h3 className="text-blue-500 font-semibold">
               {teamMembers[0].role}
             </h3>
-            <p className="text-gray-700 mb-6">{teamMembers[0].bio}</p>
+            <p className="text-gray-700 text-xs">
+            {/* {teamMembers[0].bio} */}
+            ForTechBD
+            </p>
           </div>
         </div>
-      </div> */}
-      <div className="grid xl:grid-cols-3 md:grid-cols-2 lg:gap-10 justify-center gap-5 bg-gray-100 flex-wrap ">
-        {teamMembers.map((member, index) => (
+      </div>
+      <div className="grid xl:grid-cols-4 md:grid-cols-2 lg:gap-6 justify-center gap-5 bg-gray-100 flex-wrap ">
+        {teamMembers.slice(1, 8).map((member, index) => (
           <div
             key={index}
             className="w-full  border  overflow-hidden hover:scale-105 duration-300 border-gray-300 rounded-lg bg-white text-center shadow-lg"
@@ -90,9 +93,9 @@ const Team = () => {
             <img
               src={member.image}
               alt={member.name}
-              className="w-full mb-5 max-h-[450px]"
+              className="w-full mb-5 max-h-[300px]"
             />
-            <div className="p-5">
+            <div className="px-5 pt-2">
               <h2 className="text-2xl font-semibold mb-2">{member.name}</h2>
               <h3 className="text-blue-500 text-lg mb-4 font-semibold">
                 {member.role}
