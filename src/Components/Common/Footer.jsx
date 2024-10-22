2639abdded6500041a9d16c252cac2b4cad47131
nimurr <nimurnerob404@gmail.com>
2024-10-22 14:30:55 +0600

    add

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -114,7 +114,7 @@ export default function FooterMain() {
             </div> */}
           </div>
 
-          <div className="px-4 py-6 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
+          <div className="px-4 py-6 bg-gray-800 dark:bg-gray-700 md:flex md:items-center md:justify-between">
             <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">
               © 2023 <a href="https://flowbite.com/">fortechbd.com™</a>. All
               Rights Reserved. Created By{" "}
